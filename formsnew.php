5d243c656ae4f060282ab2c41969b55fea114ae2
Jojo <jbahil47@gmail.com>
2022-04-14 20:56:30 +0800

    modified the formsnew.php

--- formsnew.php
+++ formsnew.php
@@ -29,6 +29,19 @@
             });
         }
     </script>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>TES - Landbank Form</title>
+        <script src="assets/js/jquery.min.js"></script>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/Contact-Form-Clean.css">
+        <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+        <link rel="stylesheet" href="assets/css/styles.css?ver=2.0">
+        <link rel="stylesheet" href="assets/css/dropdown.css">
+        <link rel="stylesheet" href="assets/css/croppie.css">
+        <link rel="stylesheet" href="assets/css/all.min.css">
+    </head>
 
     <body style="background-image:url('image/background2.png');background-attachment: fixed; background-position: center;">
         <?php
